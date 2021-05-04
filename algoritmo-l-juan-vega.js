8d1f370350f3ed5396e6631d731529b086581a62
juangvd <77943731+juangvd@users.noreply.github.com>
2021-05-04 04:42:35 -0400

    Add files via upload

---
+++ algoritmo-l-juan-vega.js
@@ -0,0 +1,100 @@
+////////////////////////////////////////////////////////////////////////////
+
+//                              Algoritmo I                             //
+
+////////////////////////////////////////////////////////////////////////////
+
+
+//  1.- 
+
+/*
+
+
+for(var i=0; i<5;i++){   
+   console.log(i);
+}
+
+
+*/
+//  2.- 
+/*
+
+
+for(var i=0; i<5; i++)
+{
+   i = i + 1;  
+   console.log(i);
+}
+
+
+
+*/
+//  3.- 
+/*
+
+
+for(var i=0; i<5; i++)
+{
+   i = i + 3; 
+   console.log(i);
+}
+
+
+*/
+//  4.- 
+/*
+
+
+function y(num1, num2){   
+   return num1+num2;
+}
+console.log(y(2,3))
+console.log(y(3,5))
+
+
+*/
+//  5.- 
+/*
+
+
+function y(num1, num2){
+   console.log(num1);   
+   return num1+num2;
+}
+console.log(y(2,3))
+console.log(y(3,5))
+
+
+*/
+//  6.- 
+
+/*
+
+
+a = 15;
+console.log(a);
+function y(a){
+   console.log(a);   
+   return a;
+}
+b = y(10);
+console.log(b);
+
+
+*/
+//  7.- 
+/*
+
+
+a = 15;
+console.log(a);
+function y(a){
+   console.log(a);   
+   return a*2;
+}
+b = y(10);
+console.log(b);
+
+
+*/
+
